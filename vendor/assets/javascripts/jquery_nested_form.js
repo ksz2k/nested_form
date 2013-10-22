5fcb828b50a8c4cd4d837cf7336c029ad3ea3b05
Krzysiek Szczuka <kszczuka@gmail.com>
2013-10-22 18:10:36 +0200

    delegate

--- vendor/assets/javascripts/jquery_nested_form.js
+++ vendor/assets/javascripts/jquery_nested_form.js
@@ -84,8 +84,8 @@
   };
 
   window.nestedFormEvents = new NestedFormEvents();
-  $('form a.add_nested_fields').live('click', nestedFormEvents.addFields);
-  $('form a.remove_nested_fields').live('click', nestedFormEvents.removeFields);
+  $(document).undelegate('form a.add_nested_fields').undelegate('form a.remove_nested_fields');
+  $(document).delegate('form a.add_nested_fields', 'click', nestedFormEvents.addFields).delegate('form a.remove_nested_fields', 'click', nestedFormEvents.removeFields);
 })(jQuery);
 
 // http://plugins.jquery.com/project/closestChild
